e4e6bbc2d79be8c32d4f3c44a6e49f0f928a724c
kiranpuli <kiranpuli070899@gmail.com>
2021-01-26 14:15:57 +0530

    :hammer: Imporved and refactored code

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,32 @@
-import React from 'react'
-import {auth,provider} from '../firebase'
-import './login.css'
-import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
-import Button from '@material-ui/core/Button';
+import React from "react";
+import { auth, provider } from "../firebase";
+import "./login.css";
+import QuestionAnswerIcon from "@material-ui/icons/QuestionAnswer";
+import Button from "@material-ui/core/Button";
 
 function Login() {
+  const signin = () => {
+    auth.signInWithPopup(provider).catch((err) => {
+      console.error(err.message);
+    });
+  };
 
-    const signin = ()=>{
-        // console.log("signin")
-        auth.signInWithPopup(provider).catch(err=>{console.error(err.message)})
-    }
+  const btn = {
+    fontSize: 120,
+    color: "#3f51b5",
+  };
 
-    const btn={ 
-        fontSize: 120,
-        color:"#3f51b5"
-    }
-
-    return (
-        <div className="login">
-            <div className="logo">
-                <QuestionAnswerIcon style={btn}/><h1>Groupie</h1>
-            </div>
-            <Button onClick={signin} variant="contained" color="primary">Sign in with Google</Button>
-        </div>
-    )
+  return (
+    <div className="login">
+      <div className="logo">
+        <QuestionAnswerIcon style={btn} />
+        <h1>Groupie</h1>
+      </div>
+      <Button onClick={signin} variant="contained" color="primary">
+        Sign in with Google
+      </Button>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,81 +1,80 @@
-import React,{useState,useEffect} from 'react'
-import {useSelector} from 'react-redux'
-import db,{auth} from '../firebase'
-import {selectUser} from '../features/userSlice'
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import db, { auth } from "../firebase";
+import { selectUser } from "../features/userSlice";
 
-import "./sidebar.css"
+import "./sidebar.css";
 
-import GroupLink from "./GroupLink"
+import GroupLink from "./GroupLink";
 
-import IconButton from '@material-ui/core/IconButton';
-import Button from '@material-ui/core/Button';
-import AddIcon from '@material-ui/icons/Add';
-import Avatar from '@material-ui/core/Avatar';
-// import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import AddIcon from "@material-ui/icons/Add";
+import Avatar from "@material-ui/core/Avatar";
 
 function Sidebar() {
-    const user = useSelector(selectUser);
-    const [groups, setGroups] = useState([])
+  const user = useSelector(selectUser);
+  const [groups, setGroups] = useState([]);
 
+  const logout = () => {
+    auth.signOut();
+  };
 
-    const logout=()=>{
-      // console.log("logout")
-      auth.signOut();
+  const btn = {
+    color: "#fff",
+    border: "#fff",
+    borderRadius: "10px",
+  };
+
+  const addGroup = () => {
+    const groupName = prompt("Enter group name!");
+    if (groupName) {
+      db.collection("groups").add({
+        groupName,
+      });
     }
+  };
 
-    const btn={
-      color:"#fff",
-      border:"#fff",
-      borderRadius:"10px"
-    }
+  useEffect(() => {
+    db.collection("groups").onSnapshot((snap) => {
+      setGroups(
+        snap.docs.map((doc) => ({
+          id: doc.id,
+          group: doc.data(),
+        }))
+      );
+    });
+  }, []);
 
-    const addGroup = ()=>{
-      const groupName = prompt("Enter group name!")
-      if(groupName){
-        db.collection("groups").add({
-          groupName
-        })
-      }
-    }
-
-    useEffect(() => {
-      db.collection("groups").onSnapshot((snap)=>{
-        setGroups(
-          snap.docs.map((doc)=>(
-            {
-              id:doc.id,
-              group:doc.data()
-            }
-          ))
-        )
-      })
-    }, [])
-
-    return (
-      <div className="sidebar">
-        <div className="sidebarHead">
-          <Avatar src={user.photo} /> 
-          <h2>{user.displayName}</h2>
-          <Button onClick={logout} variant="contained"  color="secondary"size="small">
-            Logout
-          </Button>
-         
-        </div>
-        <div className="sidebarBody">
-          <h3>
-            Groups 
-            <IconButton >
-              <AddIcon className="addicon" onClick={addGroup}/>
-            </IconButton>
-          </h3>
-          <div className="groupList">
-            {groups.map(({id,group})=>(
-              <GroupLink key={id} id={id} groupName={group.groupName}/>
-            ))}
-          </div>
+  return (
+    <div className="sidebar">
+      <div className="sidebarHead">
+        <Avatar src={user.photo} />
+        <h2>{user.displayName}</h2>
+        <Button
+          onClick={logout}
+          variant="contained"
+          color="secondary"
+          size="small"
+        >
+          Logout
+        </Button>
+      </div>
+      <div className="sidebarBody">
+        <h3>
+          Groups
+          <IconButton>
+            <AddIcon className="addicon" onClick={addGroup} />
+          </IconButton>
+        </h3>
+        <div className="groupList">
+          {groups.map(({ id, group }) => (
+            <GroupLink key={id} id={id} groupName={group.groupName} />
+          ))}
         </div>
       </div>
-    )
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
